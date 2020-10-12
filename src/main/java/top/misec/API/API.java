b6ceaa3d27e28d4b9d5244f09f4a4fc4934d4b3d
JunzhouLiu <junzhou2016@outlook.com>
2020-10-12 19:55:41 +0800

    调整API列表,增加投币，确认是否投过币

---
+++ src/main/java/top/misec/API/API.java
@@ -0,0 +1,14 @@
+package top.misec.API;
+
+/**
+ * @author Junzhou Liu
+ * @create 2020/10/11 3:40
+ */
+public class API {
+    public static String _LOGIN = "https://api.bilibili.com/x/web-interface/nav";
+    public static String _Manga = "https://manga.bilibili.com/twirp/activity.v1.Activity/ClockIn";
+    public static String _AvShare = "https://api.bilibili.com/x/web-interface/share/add";
+    public static String _CoinAdd = "https://api.bilibili.com/x/web-interface/coin/add";
+    public static String _isCoin = "https://api.bilibili.com/x/web-interface/archive/coins";
+
+}
