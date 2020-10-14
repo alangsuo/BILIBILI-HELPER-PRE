a9643fdadc61a5a7528d75947e55538b09fceb2c
JunzhouLiu <Junzhou2016@outlook.com>
2020-10-14 14:58:39 +0800

    对常用数据的API查询进行封装（对时效要求比较高的数据）
    Signed-off-by: JunzhouLiu <Junzhou2016@outlook.com>

--- src/main/java/top/misec/API/API.java
+++ src/main/java/top/misec/API/API.java
@@ -18,4 +18,8 @@ public class API {
     public static String silver2coin = "https://api.live.bilibili.com/pay/v1/Exchange/silver2coin";
     //状态
     public static String getSilver2coinStatus = "https://api.live.bilibili.com/pay/v1/Exchange/getStatus";
+
+    public static String videoHeartbeat = "http://api.bilibili.com/x/click-interface/web/heartbeat";
+
+    public static String getCoinBalance = "https://account.bilibili.com/site/getCoin";//查询主站硬币数量
 }
