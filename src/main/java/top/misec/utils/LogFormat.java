90559957d9264edca62d54e0a98425a95046cfa5
JunzhouLiu <Junzhou2016@outlook.com>
2020-11-21 16:10:39 +0800

    日志优化

--- src/main/java/top/misec/utils/LogFormat.java
+++ src/main/java/top/misec/utils/LogFormat.java
@@ -3,6 +3,9 @@ package top.misec.utils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author Junzhou Liu
  * @create 2020/11/21 15:31
@@ -19,4 +22,10 @@ public class LogFormat {
         logger.info("-----任务结束-----\n");
     }
 
+    public static void printTime() {
+        Date d = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String time = sdf.format(d);
+        logger.info(time);
+    }
 }
