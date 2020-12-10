d1831c2f817c80b0b6ac77fb31cd798ceb066722
JunzhouLiu <Junzhou2016@outlook.com>
2020-12-10 21:28:10 +0800

    一些细节调整。

--- src/main/java/top/misec/BiliMain.java
+++ src/main/java/top/misec/BiliMain.java
@@ -1,7 +1,6 @@
 package top.misec;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Logger;
+import lombok.extern.log4j.Log4j2;
 import top.misec.config.Config;
 import top.misec.login.ServerVerify;
 import top.misec.login.Verify;
@@ -13,15 +12,15 @@ import top.misec.utils.VersionInfo;
  * @author Junzhou Liu
  * @create 2020/10/11 2:29
  */
-public class BiliMain {
 
-    static Logger logger = (Logger) LogManager.getLogger(BiliMain.class.getName());
+@Log4j2
+public class BiliMain {
 
     public static void main(String[] args) {
 
         if (args.length < 3) {
-            logger.info("任务启动失败");
-            logger.warn("Cookies参数缺失，请检查是否在Github Secrets中配置Cookies参数");
+            log.info("任务启动失败");
+            log.warn("Cookies参数缺失，请检查是否在Github Secrets中配置Cookies参数");
         }
         //读取环境变量
         Verify.verifyInit(args[0], args[1], args[2]);
@@ -37,7 +36,7 @@ public class BiliMain {
             DailyTask dailyTask = new DailyTask();
             dailyTask.doDailyTask();
         } else {
-            logger.info("自定义配置中开启了跳过本日任务，本日任务跳过，如果需要取消跳过，请将skipDailyTask值改为0");
+            log.info("自定义配置中开启了跳过本日任务，本日任务跳过，如果需要取消跳过，请将skipDailyTask值改为0");
         }
     }
 
