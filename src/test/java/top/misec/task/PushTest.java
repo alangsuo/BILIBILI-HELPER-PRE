acbbcd27a7d391a2d0bafbd127098d4c2c5fa4df
itning <itning@itning.top>
2021-09-21 14:31:14 +0800

    test: One Test More Bugs

--- src/test/java/top/misec/task/PushTest.java
+++ src/test/java/top/misec/task/PushTest.java
@@ -1,11 +1,11 @@
 package top.misec.task;
 
-import org.apache.commons.lang3.RandomStringUtils;
-import top.misec.push.impl.DingTalkSecretPush;
+import org.junit.jupiter.api.Test;
+import top.misec.push.impl.*;
 import top.misec.push.model.PushMetaInfo;
 import top.misec.push.model.PushResult;
 
-import java.nio.charset.StandardCharsets;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * push test .
@@ -13,17 +13,102 @@ import java.nio.charset.StandardCharsets;
  * @author itning
  * @since 2021/3/28 16:06
  */
+
 public class PushTest {
-    //@Test
-    public void test1() {
-        String s = RandomStringUtils.randomAlphanumeric(19926);
-        System.out.println(s.getBytes(StandardCharsets.UTF_8).length);
+
+    @Test
+    public void testServerChanTurboPush() {
 
         PushMetaInfo pushMetaInfo = PushMetaInfo.builder()
+                .chatId("")
                 .token("")
                 .secret("")
                 .build();
-        PushResult pushResult = new DingTalkSecretPush().doPush(pushMetaInfo, s);
-        System.out.println(pushResult.isSuccess());
+        PushResult pushResult = new ServerChanTurboPush().doPush(pushMetaInfo, "testServerChanTurboPush");
+        assertTrue(pushResult.isSuccess());
+    }
+
+    @Test
+    public void testDingTalkPush() {
+
+        PushMetaInfo pushMetaInfo = PushMetaInfo.builder()
+                .chatId("")
+                .token("")
+                .secret("")
+                .build();
+        PushResult pushResult = new DingTalkPush().doPush(pushMetaInfo, "testDingTalkPush");
+        assertTrue(pushResult.isSuccess());
+    }
+
+    @Test
+    public void testDingTalkSecretPush() {
+
+        PushMetaInfo pushMetaInfo = PushMetaInfo.builder()
+                .chatId("")
+                .token("")
+                .secret("")
+                .build();
+        PushResult pushResult = new DingTalkSecretPush().doPush(pushMetaInfo, "testDingTalkSecretPush");
+        assertTrue(pushResult.isSuccess());
+    }
+
+    @Test
+    public void testPushPlusPush() {
+
+        PushMetaInfo pushMetaInfo = PushMetaInfo.builder()
+                .chatId("")
+                .token("")
+                .secret("")
+                .build();
+        PushResult pushResult = new PushPlusPush().doPush(pushMetaInfo, "testPushPlusPush");
+        assertTrue(pushResult.isSuccess());
+    }
+
+    @Test
+    public void testServerChanPush() {
+
+        PushMetaInfo pushMetaInfo = PushMetaInfo.builder()
+                .chatId("")
+                .token("")
+                .secret("")
+                .build();
+        PushResult pushResult = new ServerChanPush().doPush(pushMetaInfo, "testServerChanPush");
+        assertTrue(pushResult.isSuccess());
+    }
+
+    @Test
+    public void testTelegramCustomUrlPush() {
+
+        PushMetaInfo pushMetaInfo = PushMetaInfo.builder()
+                .chatId("")
+                .token("")
+                .secret("")
+                .build();
+        PushResult pushResult = new TelegramCustomUrlPush().doPush(pushMetaInfo, "testTelegramCustomUrlPush");
+        assertTrue(pushResult.isSuccess());
+    }
+
+    @Test
+    public void testTelegramPush() {
+
+        PushMetaInfo pushMetaInfo = PushMetaInfo.builder()
+                .chatId("")
+                .token("")
+                .secret("")
+                .build();
+        PushResult pushResult = new TelegramPush().doPush(pushMetaInfo, "testTelegramPush");
+        assertTrue(pushResult.isSuccess());
+    }
+
+    @Test
+    public void testWeComPush() {
+
+        PushMetaInfo pushMetaInfo = PushMetaInfo.builder()
+                .chatId("")
+                .token("")
+                .secret("")
+                .build();
+        PushResult pushResult = new WeComPush().doPush(pushMetaInfo, "testWeComPush");
+        assertTrue(pushResult.isSuccess());
     }
 }
